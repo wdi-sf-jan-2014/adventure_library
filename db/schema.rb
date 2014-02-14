09e9f7fea562fd7b0f0785362235f777574e9e3b
cbestiva <cbestiva@gmail.com>
2014-02-13 18:58:02 -0800

    added guid to adventure model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140214023121) do
+ActiveRecord::Schema.define(version: 20140214025719) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20140214023121) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "library_id"
+    t.string   "guid"
   end
 
   create_table "libraries", force: true do |t|
