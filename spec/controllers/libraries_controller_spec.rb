bb8a1ec2eebafb1b625c1ac03d139b5ee27efe7c
Tanner Swenson <TannerSwenson@gmail.com>
2014-02-14 11:08:08 -0800

    passed libraries index and new specs

--- spec/controllers/libraries_controller_spec.rb
+++ spec/controllers/libraries_controller_spec.rb
@@ -24,43 +24,43 @@ describe LibrariesController do
     end
   end
 
-  # describe "GET 'index'" do
-  #   before do
-  #     @libraries = []
-  #     for i in 1..5 do
-  #       @libraries << Library.create(url: "google.com#{i}")
-  #     end
-  #   end
-  #   it "returns http success for html " do
-  #     get 'index'
-  #     response.status.should == 200
-  #   end
-  #   it "includes libaries" do
-  #     get 'index'
-  #     response.body.should include(@libraries[0])
-  #     response.body.should include(@libraries[3])
-  #   end
+  describe "GET 'index'" do
+    before do
+      @libraries = []
+      for i in 1..5 do
+        @libraries << Library.create(url: "google.com#{i}")
+      end
+    end
+    it "returns http success for html " do
+      get 'index'
+      response.status.should == 200
+    end
+    it "includes libaries" do
+      get 'index'
+      response.body.should include(@libraries[0].url)
+      response.body.should include(@libraries[3].url)
+    end
 
-  #   it "returns http success for json" do
-  #     get 'index.json'
-  #     response.status.should == 200
-  #   end
-  #   it "includes libraries for json" do
-  #     get 'index.json'
-  #     response.body.should include @libraries[3].to_json
-  #     response.body.should include @libraries[1].to_json
-  #   end
-  # end
+    it "returns http success for json" do
+      get 'index', {format: :json}
+      response.status.should == 200
+    end
+    it "includes libraries for json" do
+      get 'index', {format: :json}
+      response.body.should include @libraries[3].to_json
+      response.body.should include @libraries[1].to_json
+    end
+  end
 
-  # describe "GET 'new'" do
-  #   it "returns http success for html" do
-  #     get 'new'
-  #     response.status.should == 200
-  #   end
-  #   it "returns 404 for json" do
-  #     get 'new.json'
-  #     response.status.should == 404
-  #   end
-  # end
+  describe "GET 'new'" do
+    it "returns http success for html" do
+      get 'new'
+      response.status.should == 200
+    end
+    it "returns 404 for json" do
+      get 'new', {format: :json}
+      response.status.should == 404
+    end
+  end
 
 end
