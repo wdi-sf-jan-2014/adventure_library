a2df7d3792688e4c2b48381ee4dafb7708fa2e49
kaifius <kai.hofius@gmail.com>
2014-02-14 13:28:15 -0800

    models generated

---
+++ db/migrate/20140214211833_create_adventures.rb
@@ -0,0 +1,12 @@
+class CreateAdventures < ActiveRecord::Migration
+  def change
+    create_table :adventures do |t|
+      t.references :library
+      t.string :title
+      t.string :author
+      t.string :GUID
+
+      t.timestamps
+    end
+  end
+end
