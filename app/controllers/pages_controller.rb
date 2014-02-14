d0b004adf6af9f8adb8c605fa63f1f7ea8ea5514
Tanner Swenson <TannerSwenson@gmail.com>
2014-02-14 15:44:48 -0800

    Basic adventures flow works

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,11 +3,18 @@ class PagesController < ApplicationController
   end
 
   def show
+    @page = Page.find(id)
   end
 
   def edit
   end
 
   def new
+  end
+
+  private
+
+  def id
+    params[:id]
   end
 end
