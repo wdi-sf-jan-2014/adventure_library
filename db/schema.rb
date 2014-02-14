ad74824562a14fecaa8c6552da01ebde6c984324
cbestiva <cbestiva@gmail.com>
2014-02-13 18:35:38 -0800

    generated and migrated library model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140214021304) do
+ActiveRecord::Schema.define(version: 20140214023121) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,13 @@ ActiveRecord::Schema.define(version: 20140214021304) do
   create_table "adventures", force: true do |t|
     t.text     "title"
     t.text     "author"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "library_id"
+  end
+
+  create_table "libraries", force: true do |t|
+    t.string   "url"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
