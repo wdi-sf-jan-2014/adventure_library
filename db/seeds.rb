6dd3bfc49435a7d20ff74c3f0448b49603145368
afleisch <afleisch@gmail.com>
2014-02-15 14:15:00 -0800

    added to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,8 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-adv = Adventure.create!(:title => "#{name}'s Test Adventure",
-	:author => name)
+adv = Adventure.create!(:title => "Ashley's Test Adventure",
+	:author => "Ashley", :guid => "#{SecureRandom.urlsafe_base64(10)}")
 adv.pages.create!(:name => "start",
 	:text => "This is a great text adventure which is beginning right now!  I can't wait to [[see the end|end]]")
 adv.pages.create!(:name => "end",
