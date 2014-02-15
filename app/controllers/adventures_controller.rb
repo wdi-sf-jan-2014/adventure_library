f4a3251d7a1f73ddaecab336e07b3048ccff3e76
lalanikki <lalanikki@gmail.com>
2014-02-15 12:18:52 -0800

    adventure index method and view

---
+++ app/controllers/adventures_controller.rb
@@ -0,0 +1,34 @@
+class AdventuresController < ApplicationController
+
+  def index
+    @local_adventures = []
+    @foreign_adventures = []
+    Adventure.all.each do |ad|
+      if ad.is_local?
+        @local_adventures << ad
+      elsif !ad.is_local?
+        @foreign_adventures << ad
+      end
+    end
+  end
+
+  def new
+    @adventure = Adventure.new
+  end
+
+  def create
+    new_adventure = params.require(:adventure).permit(:library_id, :title, :author)
+    adventure = Adventure.create(new_adventure)
+    if adventure
+      redirect_to adventure
+    else
+      render :new
+    end
+  end
+
+  def show
+    @adventure = Adventure.find(params[:id])
+    @start = @adventure.pages.where(:name => "start")
+  end
+
+end
