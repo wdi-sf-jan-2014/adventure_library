fa76370488568cfdc4393bf52f59126ba477e5a9
darrenwong916 <darrenwong06@gmail.com>
2014-02-15 15:46:30 -0800

    added forms for pages/new and defined new method in pages controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,6 +3,9 @@ class PagesController < ApplicationController
   end
 
   def new
+    id = params[:adventure_id]
+    find_page = Adventure.find(id)
+    @adventure = find_page
   end
 
   def create
