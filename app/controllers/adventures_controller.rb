940f1d51ff764d52d7008f8fa79e3fef7e278167
SylvieC <sylviecottrell@gmail.com>
2014-02-15 15:26:51 -0800

    simplified the worker file

--- app/controllers/adventures_controller.rb
+++ app/controllers/adventures_controller.rb
@@ -13,6 +13,7 @@ class AdventuresController < ApplicationController
       new_adventure= params.require(:adventure).permit(:title, :author)
       new_adventure = Adventure.create(new_adventure)
       new_adventure.guid = SecureRandom.urlsafe_base64(10)
+      new_adventure.save
       @adventure = new_adventure
       redirect_to new_adventure_page_path(@adventure.id)
     end
