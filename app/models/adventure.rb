df7593271bbaf395a26078b3648ce0dca7807d1c
lalanikki <lalanikki@gmail.com>
2014-02-15 12:35:48 -0800

    fix view/ad/new formatting and added guid to adventure model

--- app/models/adventure.rb
+++ app/models/adventure.rb
@@ -2,7 +2,17 @@ class Adventure < ActiveRecord::Base
   has_many :pages
   belongs_to :library
 
-  def is_local? adventure
-    adventure.library_id.nil?
+  before_create :create_guid
+  
+  private
+
+  def create_guid
+    if self.guid.nil?
+      self.guid = SecureRandom.urlsafe_base64(10)
+    end
+  end
+
+  def is_local?
+    self.library_id.nil?
   end
 end
