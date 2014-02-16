cd7a4ae862fd1b281d14017612486cb792bca1da
SylvieC <sylviecottrell@gmail.com>
2014-02-15 20:08:28 -0800

    refactor Worker file

--- app/workers/library_worker.rb
+++ app/workers/library_worker.rb
@@ -4,19 +4,19 @@ class LibraryWorker
    def perform(library_id)
     url = Library.find(library_id).url
           response = Typhoeus.get( url + "/adventures.json")
-          result = response1.body.as_json
+          result = response.body.as_json
           
-          result['adventures'] = answer["adventures"]
+  
           result["adventures"].each do |adventure|
             library_to_search.adventures << Adventure.create(adventure)
           end
 
 
 
-          response2 = Typhoeus.get(url +"/libraries.json")
-          result = response2.body.as_json
-          result["libraries"] = answer["libraries"]
+          response = Typhoeus.get(url +"/libraries.json")
+          result = response.body.as_json
           @libraries = Library.all
+
           result['libraries'].each do |library|
             if !@libraries.include?(library.url)
               Library.create(url:  library.url)
