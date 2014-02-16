230b194e77c4ad20ff6a255268a6148b308b3a9c
lalanikki <lalanikki@gmail.com>
2014-02-16 01:12:25 -0800

    added root

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 AdventureLibrary::Application.routes.draw do
+  
+  root to: "adventures#index"
+
   resources :libraries, only: [:index, :new, :create]
   resources :adventures do
     resources :pages, except: [:index, :edit, :update]
   end
+
+  
+
 end
