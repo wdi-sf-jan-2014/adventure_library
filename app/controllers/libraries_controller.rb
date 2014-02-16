e878e0731610d523a224e24d9229ecef0437fd1b
nathantross <nathantross@gmail.com>
2014-02-15 20:43:39 -0800

    Added JSON to library controller

--- app/controllers/libraries_controller.rb
+++ app/controllers/libraries_controller.rb
@@ -2,7 +2,14 @@ class LibrariesController < ApplicationController
 
   def index
     @libraries = Library.all
+
+    respond_to do |format|
+      format.html
+      format.json { render :json => { :libraries => @libraries.as_json( only: :url )}}
+    end
+
   end
+
 
   def new
     @library = Library.new
