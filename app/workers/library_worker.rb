f11b00c9db99198692b0d738dc8db42e03b71740
SylvieC <sylviecottrell@gmail.com>
2014-02-15 17:53:41 -0800

    sidekiq worker doing the job but still getting an error

--- app/workers/library_worker.rb
+++ app/workers/library_worker.rb
@@ -3,15 +3,19 @@ class LibraryWorker
 
    def perform(library_id)
     url = Library.find(library_id).url
-          response = Typhoeus.get( url + "/adventures")
-          result = response.body
-          adventure_array = result["adventures"]
-          adventure_array.each do |adventure|
-              library_to_search.adventures << Adventure.create(adventure)
+          response1 = Typhoeus.get( url + "/adventures.json")
+          answer = response1.body.as_json
+          result = {}
+          result['adventures'] = answer["adventures"]
+          result["adventures"].each do |adventure|
+            library_to_search.adventures << Adventure.create(adventure)
           end
 
-          response = Typhoeus.get(url +"/libraries")
-          result = response.body
+
+
+          response2 = Typhoeus.get(url +"/libraries.json")
+          answer = response2.body.as_json
+          result["libraries"] = answer["libraries"]
           @libraries = Library.all
           result['libraries'].each do |library|
             if !@libraries.include?(library.url)
