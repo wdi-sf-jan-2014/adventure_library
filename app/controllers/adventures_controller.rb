3206503d94d8436d90caa9b05241cdd38ad472cc
afleisch <afleisch@gmail.com>
2014-02-15 19:46:04 -0800

    added actions to adventures

--- app/controllers/adventures_controller.rb
+++ app/controllers/adventures_controller.rb
@@ -1,6 +1,24 @@
 class AdventuresController < ApplicationController
 
   def index
+    @adventures = Adventure.all
+  end
+
+  def new
+    @adventure = Adventure.new
+    guid = SecureRandom.urlsafe_base64(10)
+  end
+
+  def show
+    adv_id = params[:id]
+    @adventure = Adventure.find(adv_id)
+    pages = @adventure.pages
+    @start = pages.find_by name: "start"
+  end
+
+  def create
+    adventure = Adventure.create
+    redirect_to adventure
   end
 
 
