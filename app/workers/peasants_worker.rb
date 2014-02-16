42a6e79f2902b79376bc2b6ad2dd7d3dfb5b833e
kevinrogerteng <kevin.roger.teng@gmail.com>
2014-02-16 11:37:05 -0800

    Got sidekiq to work

--- app/workers/peasants_worker.rb
+++ app/workers/peasants_worker.rb
@@ -1,10 +1,17 @@
 class PeasantsWorker
 	include Sidekiq::Worker
 
-	def perform (library_id)
-		library = Library.find(library_id)
-		result = Typhoeus.get(open(library.url))
-		binding.pry
+	def perform(library_id)
+		new_library = Library.find(library_id)
+		response = Typhoeus.get("#{new_library.url}adventures.json")
+        result = JSON.parse(response.body)
+        result["adventures"].each do |adventure|
+          obtained_adventure = new_library.adventures.create!(:title => adventure["title"], :author => adventure["author"], :guid => adventure["guid"])
+      
+            adventure["pages"].each do |page|
+              obtained_pages = obtained_adventure.pages.create!(:name => page["name"], :text => page["text"])
+            end
+        end
 
 	end
 
