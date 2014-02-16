509c56a427e6d29103cabd950f052ae53bc77e15
mbetts7 <mbetts7@gmail.com>
2014-02-16 10:55:31 -0800

    hopeufully fixed seed file for good

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,8 @@
-adv = Adventure.create!(:title => "Mike B's Test Adventure",
+name = "Mike B"
+
+library = Library.create!(url: "http://stormy-sands-1901.herokuapp.com", name: "#{name}'s Library")
+
+adv = library.adventures.create!(:title => "#{name}'s Terrible Adventure Story",
 	:author => "Mike B")
 adv.pages.create!(:name => "start",
 	:text => "Adventure stories, in general, suck!  So better get on with it... [[go to pg1|pg1]]")
@@ -8,6 +12,3 @@ adv.pages.create!(:name => "pg2",
   :text => "This is a great text adventure which I'm going to end about now... [[see the end|end]]")
 adv.pages.create!(:name => "end",
 	:text => "Wow that adventure was amazing!")
-
-adv.create_library(url: "http://stormy-sands-1901.herokuapp.com", name: "Mike's library")
-
