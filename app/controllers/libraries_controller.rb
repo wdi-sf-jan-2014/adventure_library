5aaa06e061ac9d26fed4290f7cf3429db1170689
calisteveo <soglesbee1@gmail.com>
2014-02-16 12:10:35 -0800

    libraries controller update

--- app/controllers/libraries_controller.rb
+++ app/controllers/libraries_controller.rb
@@ -1,20 +1,29 @@
 class LibrariesController < ApplicationController
 
+  include LibrariesHelper
+
   def new
     @library = Library.new
   end
 
   def index
     @library = Library.all
-  end
-
-  def show
-    @library = Library.find(params[:id])
-    @adventures = @library.adventures
     respond_to do |f|
       f.html
-      f.json { render :json => @library}
+      f.json { render :json => { libraries: @libraries.as_json } }
+    end
   end
 
- end
+  def create
+    lib = Library.create(url: params[:library][:url])
+    new_adv = Typhoeus.get(params[:library][:url])
+    resp = JSON.parse(new_adv.body)["adventures"]
+    resp.each do |x|
+      adv = lib.adventures.create(title: x["title"], author: x["author"], guid: x["guid"])
+      x["pages"].each do |y|
+        adv.pages.create(name: y["name"], text: y["text"])
+      end
+    end
+    redirect_to libraries_path
+  end
 end
