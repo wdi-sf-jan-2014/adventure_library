58fa108dda43d168389942ec2c01c8271b489238
smarsh25 <sjm.atl@gmail.com>
2014-02-16 11:30:55 -0800

    correcting json format for adventures, now passing rspec

--- app/controllers/adventures_controller.rb
+++ app/controllers/adventures_controller.rb
@@ -7,9 +7,14 @@ class AdventuresController < ApplicationController
     local_adventures = Adventure.where("library_id" => nil)
     respond_to do |format|
       format.html
-      format.json {render :json => local_adventures.to_json(except: [:id, :library_id],
-                                      include: {pages: {except: [:id, :adventure_id]}} )}
+      format.json {render :json => {:adventures => local_adventures.as_json(except: [:id, :library_id], include: {pages: {except: [:id, :adventure_id]}} )}}
     end
   end
 
 end
+
+
+# respond_to do |f|
+#       f.html { render :index }
+#       f.json { render :json => {:adventures => @local_adventures.as_json(only: [:title, :author, :guid, :updated_at], include: { :pages => {only: [:name, :text]} })} }
+#     end
