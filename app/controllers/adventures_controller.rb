acd0344ca75703a5bd4958bf4a13534e8ae34905
afleisch <afleisch@gmail.com>
2014-02-16 15:02:31 -0800

    added json to adventures#index

--- app/controllers/adventures_controller.rb
+++ app/controllers/adventures_controller.rb
@@ -3,6 +3,14 @@ class AdventuresController < ApplicationController
 
   def index
     @adventures = Adventure.all
+    @local_adventures = Adventure.where(library_id: nil)
+
+    respond_to do |format|
+      format.html
+      format.json { render json: {adventures: @local_adventures.as_json(
+                                  except: [:id, :library_id],
+                                  include: {:pages => {except: :id}})} }
+    end
   end
 
   def new
@@ -21,8 +29,5 @@ class AdventuresController < ApplicationController
     pages = @adventure.pages
     @start = pages.find_by name: "start"
   end
-
-
-
-
+  
 end
