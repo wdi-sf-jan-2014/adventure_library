8f36859b92fd485275d96e21838077d2a147e1b5
kaifius <kai.hofius@gmail.com>
2014-02-15 16:31:47 -0800

    can navigate pages

--- app/controllers/libraries_controller.rb
+++ app/controllers/libraries_controller.rb
@@ -1,5 +1,11 @@
-LibrariesController < ApplicationController
+class LibrariesController < ApplicationController
 
   def index
+    @libraries = Library.all
+
+    respond_to do |format|
+      format.html
+      format.json { render json: { libraries: @libraries } }
+    end
   end
 end
