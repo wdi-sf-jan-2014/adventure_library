092ab331f7ff9d5c0f6ade76e2d9a84769ef660c
afleisch <afleisch@gmail.com>
2014-02-15 23:53:09 -0800

    added pages#show action

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,2 +1,11 @@
 class PagesController < ApplicationController
+
+
+  def show
+    adventure_id = params[:adventure_id]
+    adventure = Adventure.find(adventure_id)
+    page_id = params[:id]
+    pages = adventure.pages
+    @page = pages.find(page_id)
+  end
 end
