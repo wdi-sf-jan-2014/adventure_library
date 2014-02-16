ed0e039ac61b6b53ba9faffcbeb18e950eca088a
darrenwong916 <darrenwong06@gmail.com>
2014-02-16 12:41:24 -0800

    made create method in library going to redis and passed library rspec

--- app/controllers/libraries_controller.rb
+++ app/controllers/libraries_controller.rb
@@ -12,5 +12,14 @@ class LibrariesController < ApplicationController
   end
 
    def create
+    if Library.where(url: params[:library]["url"]) == []
+      create_library = params.require(:library).permit(:url)
+      @library = Library.create(create_library)
+      AdventuresWorker.perform_async(@library.id)
+      LibrariesWorker.perform_async(@library.id)
+      redirect_to adventures_path
+    else
+      redirect_to libraries_path
+    end
    end
 end
