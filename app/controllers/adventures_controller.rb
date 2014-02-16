99590198ace72f9a464880c08a959bdf4abd9ca9
smarsh25 <sjm.atl@gmail.com>
2014-02-15 21:57:39 -0800

    adding return only local adventures for json

--- app/controllers/adventures_controller.rb
+++ app/controllers/adventures_controller.rb
@@ -2,10 +2,13 @@ class AdventuresController < ApplicationController
 
   def index
     @adventures = Adventure.all
+
+    # get adventures that were created from this site
+    local_adventures = Adventure.where("library_id" => nil)
     respond_to do |format|
       format.html
-      format.json {render :json => @adventures.to_json(except: [:id, :library_id],
-                                      include: :pages)}
+      format.json {render :json => local_adventures.to_json(except: [:id, :library_id],
+                                      include: {pages: {except: [:id, :adventure_id]}} )}
     end
   end
 
