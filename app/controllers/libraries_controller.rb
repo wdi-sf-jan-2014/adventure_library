341ff9514db725eea9295ba0b3d9b1eddf37d571
smarsh25 <sjm.atl@gmail.com>
2014-02-16 14:06:35 -0800

    adding libraries json

--- app/controllers/libraries_controller.rb
+++ app/controllers/libraries_controller.rb
@@ -2,6 +2,12 @@ class LibrariesController < ApplicationController
 
   def index
     @libraries = Library.all
+
+    respond_to do |format|
+      format.html
+      format.json {render :json => {:libraries => @libraries.as_json(except: [:id])}}
+    end
+
   end
 
 end
