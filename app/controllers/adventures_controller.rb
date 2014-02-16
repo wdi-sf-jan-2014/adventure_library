1c866a59f9409615578775aeb0f14936816c863f
afleisch <afleisch@gmail.com>
2014-02-15 22:08:52 -0800

    changed params for adventure#create

--- app/controllers/adventures_controller.rb
+++ app/controllers/adventures_controller.rb
@@ -1,16 +1,16 @@
 class AdventuresController < ApplicationController
 
+
   def index
     @adventures = Adventure.all
   end
 
   def new
     @adventure = Adventure.new
-    guid = SecureRandom.urlsafe_base64(10)
   end
 
   def create
-    new_adventure = params.require(:adventure).permit(:title, :author, :guid, :pages_attributes=>[:name, :text])
+    new_adventure = params.require(:adventure).permit(:title, :author, :pages_attributes=>[:name, :text])
     adventure = Adventure.create(new_adventure)
     redirect_to adventure
   end
