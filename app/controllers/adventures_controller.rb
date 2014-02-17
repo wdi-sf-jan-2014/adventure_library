b64a416665edc8460d5b667ec1668a736436c92e
melodicodyssey <stevedbauer@gmail.com>
2014-02-16 23:04:12 -0800

    after push to heroku

--- app/controllers/adventures_controller.rb
+++ app/controllers/adventures_controller.rb
@@ -1,8 +1,8 @@
 class AdventuresController < ApplicationController
 
   def index
-    # libraries = JSON.parse(Typhoeus.get("http://adventures-with-raphael.herokuapp.com/libraries.json").body)['libraries']
-    AdventuresWorker.perform_async("http://adventures-with-raphael.herokuapp.com/")
+    libraries = JSON.parse(Typhoeus.get("http://adventures-with-raphael.herokuapp.com/libraries.json").body)['libraries']
+    AdventuresWorker.perform_async(libraries)
     @all_adventures = Adventure.all
     @adventures = Adventure.where(library_id: nil)
     @other_adventures = Adventure.where(library_id: !nil)
--- app/workers/adventures_worker.rb
+++ app/workers/adventures_worker.rb
@@ -3,25 +3,13 @@ class AdventuresWorker
 
 
   def perform (url)
-    response = JSON.parse(Typhoeus.get(url+"/libraries.json").body)['libraries']
-    # unless Library.find_by(url: url)
-    #   library = Library.create(url: url)
-    # else
-    #   library = Library.find_by(url: url)
-    # end
-
-    unless Library.find_by(url: url)
-      Library.create(url: url)
-    end
-
-    get_adventures(url)
-    # adventures = JSON.parse(Typhoeus.get(url+"/adventures.json").body)['adventures']
-    # adventures.each do |adventure|
-    #   library.adventures << Adventure.create(adventure)
-    # end
+    request = "#{url}/libraries.json"
+    response = JSON.parse(Typhoeus.get(request).body)['libraries']
 
     response.each do |lib|
-    perform(lib['url'])
+      Library.create(url: lib['url']) unless Library.find_by(url: lib['url'])
+      get_adventures(lib['url'])
+      perform(lib['url'])
     end
   end
 
@@ -30,33 +18,37 @@ class AdventuresWorker
     
     # find library in database
     library = Library.find_by(url: lib_url)
-    
-    # request JSON info: adventures on server
-    response = JSON.parse(Typhoeus.get(lib_url+"/adventures.json").body)['adventures']
-    
-    # either update adventures to database ...
-    response.each do |adv|
-      adventure = Adventure.find_by(guid: adv['guid'])
-      if adventure
-        adventure.update_attributes(title: adv['title'], author: adv['author'])
-        
-        # add/update pages to database
-        adv['pages'].each do |page|
-          if Page.find_by(name: page['name'])
-            adventure.pages.find_by(name: page['name']).update_attributes(text: page['text'])
-          else
-            adventure.pages << Page.create(name: page['name'], text: page['text'])
-          end
-        end
 
-      # ... or add adventures to database
-      else
-        new_adv = Adventure.create(title: adv['title'], author: adv['author'], guid: adv['guid'])
-        library.aventures << new_adv
+    # request JSON info: adventures on server
+    request = "#{lib_url}/adventures.json"
+    response = JSON.parse(Typhoeus.get(request).body)['adventures']
+    
+    response.each do |adv|
+      
+      # either update adventures to database ...
+      # if Adventure.find_by(guid: adv['guid'])
+      #   adventure = Adventure.find_by(guid: adv['guid'])
+      #   adventure.update_attributes(title: adv['title'], author: adv['author'])
+        
+      #   # add/update pages to database
+      #   adv['pages'].each do |page|
+      #     if Page.find_by(name: page['name'])
+      #       adventure.pages.find_by(name: page['name']).update_attributes(text: page['text'])
+      #     else
+      #       adventure.pages << Page.create!(name: page['name'], text: page['text'])
+      #     end
+      #   end
+
+      # # ... or add adventures to database
+      # else
+        unless Adventure.find_by(guid: adv['guid'])
+        new_adv = library.adventures.build(:title => adv['title'], :author => adv['author'], :guid => adv['guid'])
+        new_adv.save
 
         # add pages to databasea
         adv['pages'].each do |page|
-          new_adv.pages << Page.create(name: page['name'], text: page['text'])
+          new_page = new_adv.pages.build(:name => page['name'], :text => page['text'])
+          new_page.save
         end
       end
     end
