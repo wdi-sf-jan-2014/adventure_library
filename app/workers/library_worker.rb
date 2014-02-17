f46b842aba149ef3031011708a10f49925683c4a
lalanikki <lalanikki@gmail.com>
2014-02-17 01:15:37 -0800

    workers fixed and added sidetiq - seems to work locally

--- app/workers/library_worker.rb
+++ app/workers/library_worker.rb
@@ -9,16 +9,20 @@ class LibraryWorker
         library = Library.new(url: l["url"])
         if library.save
           adv_response = Typhoeus.get("#{library.url}"+"/adventures.json")
-          adventure_hash = JSON.parse(adv_response.body)
-          adventure_hash["adventures"].each do |a|
-            if Adventure.find_by(guid: a["guid"])
-              next
-            else
-              adventure = library.adventures.build(:title => a["title"], :author => a["author"], :guid => a["guid"])
-              if adventure.save
-                a["pages"].each do |p|
-                  page = adventure.pages.build(:name => p["name"], :text => p["text"])
-                  page.save
+          if adv_response.code == 0
+            next
+          else
+            adventure_hash = JSON.parse(adv_response.body)
+            adventure_hash["adventures"].each do |a|
+              if Adventure.find_by(guid: a["guid"])
+                next
+              else
+                adventure = library.adventures.build(:title => a["title"], :author => a["author"], :guid => a["guid"])
+                if adventure.save
+                  a["pages"].each do |p|
+                    page = adventure.pages.build(:name => p["name"], :text => p["text"])
+                    page.save
+                  end
                 end
               end
             end
@@ -27,5 +31,4 @@ class LibraryWorker
       end
     end
   end
-
 end
--- app/workers/updates_worker.rb
+++ app/workers/updates_worker.rb
@@ -2,13 +2,35 @@ class UpdatesWorker
   include Sidekiq::Worker
   include Sidetiq::Schedulable
 
-  recurrence { hourly.minute_of_hour(15, 45) }
+  recurrence { hourly.minute_of_hour(7, 14, 28, 35, 42, 49, 56) }
 
   def perform
     libraries = Library.all
+    for_worker = []
     libraries.each do |library|
-      Library.create(library.url)
+      response = Typhoeus.get("#{library.url[0..-2]}"+"/adventures.json")
+      if response.code == 0
+        next
+      else
+        adventures = JSON.parse(response.body) 
+        adventures["adventures"].each do |a|
+          if Adventure.find_by(guid: a["guid"]) && (Adventure.find_by(guid: a["guid"]))["created_at"] < a["updated_at"]
+            adv = Adventure.find_by(guid: a["guid"])
+            adv.update_attributes(:title => a["title"],:author => a["author"])
+          elsif !Adventure.find_by(guid: a["guid"])
+            new_a = library.adventures.build(:title => a["title"], :author => a["author"], :guid => a["guid"])
+            if new_a.save
+              a["pages"].each do |p|
+                page = adventure.pages.build(:name => p["name"], :text => p["text"])
+                page.save
+              end
+            end
+          end
+        end
+        for_worker << { "url" => library.url[0..-2]}
+      end
     end
+    LibraryWorker.perform_async(for_worker)
   end
 
 end
