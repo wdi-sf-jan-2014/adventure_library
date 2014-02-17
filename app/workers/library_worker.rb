eb42da2cd98386046137e6b6fd5ddafe40f6360c
lalanikki <lalanikki@gmail.com>
2014-02-17 02:01:53 -0800

    fixed logic so gets new adventures of existing libs

--- app/workers/library_worker.rb
+++ app/workers/library_worker.rb
@@ -3,30 +3,26 @@ class LibraryWorker
 
   def perform (libraries)
     libraries.each do |l|
-      if Library.find_by(url: l["url"])
-        next
-      else
-        library = Library.new(url: l["url"])
-        if library.save
-          adv_response = Typhoeus.get("#{library.url}"+"/adventures.json")
-          if adv_response.code == 0
-            next
-          else
-            adventure_hash = JSON.parse(adv_response.body)
-            adventure_hash["adventures"].each do |a|
-              if Adventure.find_by(guid: a["guid"])
-                next
-              else
-                adventure = library.adventures.build(:title => a["title"], :author => a["author"], :guid => a["guid"])
-                if adventure.save
-                  a["pages"].each do |p|
-                    page = adventure.pages.build(:name => p["name"], :text => p["text"])
-                    page.save
-                  end
+      library = Library.find_or_create_by(url: l["url"])
+        adv_response = Typhoeus.get("#{library.url}"+"/adventures.json")
+        if adv_response.code == 0
+          next
+        else
+          adventure_hash = JSON.parse(adv_response.body)
+          adventure_hash["adventures"].each do |a|
+            if Adventure.find_by(guid: a["guid"])
+              next
+            else
+              adventure = library.adventures.build(:title => a["title"], :author => a["author"], :guid => a["guid"])
+              if adventure.save
+                a["pages"].each do |p|
+                  page = adventure.pages.build(:name => p["name"], :text => p["text"])
+                  page.save
                 end
               end
             end
           end
+        end
         end
       end
     end
