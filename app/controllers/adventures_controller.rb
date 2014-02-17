c5253e3f1f41d125812379f09a675eed6b9ca0f7
yutaka_houlette <yutakahoulette@gmail.com>
2014-02-16 16:23:02 -0800

    added form to add urls

--- app/controllers/adventures_controller.rb
+++ app/controllers/adventures_controller.rb
@@ -1,8 +1,9 @@
 class AdventuresController < ApplicationController
   
   def index
-  @adventure = Adventure.all
-  my_library = Library.first 
+  @adventures = Adventure.all
+  @remote_adventures = Library.new
+  my_library = Library.all
     respond_to do |f|
     f.html
     f.json { render :json => my_library.to_json(only: [:url, :name], include: [ adventures:
@@ -10,16 +11,23 @@ class AdventuresController < ApplicationController
       [ pages: {only: [:name, :text]}]}])}
     end
   end
-
+  
   def new
   @adventure = Adventure.new
   end
 
   def create
-  library = Library.create!(url: "/adventures", name: "Yutaka's library")
-  adventure = library.adventures.create!(params[:adventure].permit(:title, 
-    :author, :guid, :pages_attributes =>[:name, :text])) 
-  redirect_to "/adventures/#{adventure.id}/pages/new"
+    if Library.first == nil
+      library = Library.create(url: "/adventures", name: "Yutaka's library")
+      adventure = library.adventures.create!(params[:adventure].permit(:title, 
+      :author, :guid, :pages_attributes =>[:name, :text])) 
+      redirect_to "/adventures/#{adventure.id}/pages/new"
+    else 
+      y_library = Library.first  
+      adventure = y_library.adventures.create(params[:adventure].permit(:title, 
+      :author, :guid, :pages_attributes =>[:name, :text])) 
+      redirect_to "/adventures/#{adventure.id}/pages/new"
+    end
   end
 
   def show
