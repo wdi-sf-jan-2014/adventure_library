a6e9c81d017f2975192bab8fb7d4c30a2294c9a8
kevinrogerteng <kevin.roger.teng@gmail.com>
2014-02-16 23:05:44 -0800

    got scrape of libraries to work a bit, need to create validations

--- app/workers/peasants_worker.rb
+++ app/workers/peasants_worker.rb
@@ -3,6 +3,21 @@ class PeasantsWorker
 
 	def perform(library_id)
 		new_library = Library.find(library_id)
+		library_response = Typhoeus.get("#{new_library.url}libraries.json)")
+	    result_lib = JSON.parse(library_response.body)
+	    result_lib["libraries"].each do |library|
+			new_library = Library.create(:url => library["url"])
+			response = Typhoeus.get("#{new_library.url}adventures.json")
+	            result = JSON.parse(response.body)
+	              result["adventures"].each do |adventure|
+	              obtained_adventure = new_library.adventures.create!(:title => adventure["title"], :author => adventure["author"], :guid => adventure["guid"])
+	        
+	                adventure["pages"].each do |page|
+	                  obtained_pages = obtained_adventure.pages.create!(:name => page["name"], :text => page["text"])
+	                end
+	            end
+	    end
+
 		response = Typhoeus.get("#{new_library.url}adventures.json")
         result = JSON.parse(response.body)
         result["adventures"].each do |adventure|
@@ -12,20 +27,5 @@ class PeasantsWorker
               obtained_pages = obtained_adventure.pages.create!(:name => page["name"], :text => page["text"])
             end
         end
-        PeasantsWorker.library_crawl_async(new_library.url)
 	end
-
-   
-    def library_crawl(something)
-        response = Typhoeus.get("#{something}libraries.json)")
-        result_lib = JSON.parse(second_response.body)["libraries"]
-        i = 0
-        while i > result_lib.length
-        	new_library = Library.create(:url => result_lib["url"])
-        	PeasantsWorker.perform_async(new_library.id)
-        	i += 1
-        end
-    end
-
-
 end
