44db73f393647c8e9f9db7582eb7300cdd3f3b36
Sterling Radcliff <radcliff@riseup.net>
2014-05-15 20:02:26 -0700

    rails generate model Page; rails generate controller pages

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,23 @@
+class PagesController < ApplicationController
+  def index
+  end
+
+  def show
+    @page = Page.find(params[:id])
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
